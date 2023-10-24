e9fe2aae7b102c6147442b4abfc5190e18c46cba
Tri Bach <tribachhumg@gmail.com>
2023-10-25 00:08:26 +0700

    Update log health API

--- src/main/java/edu/petstore/pet/api/HealthController.java
+++ src/main/java/edu/petstore/pet/api/HealthController.java
@@ -1,6 +1,5 @@
 package edu.petstore.pet.api;
 
-import java.net.InetAddress;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class HealthController {
   @GetMapping
-  public ResponseEntity<Object> health() {
-    return ResponseEntity.ok()
-        .body(
-            "Spring app say hello from IP: "
-                + InetAddress.getLoopbackAddress().getHostAddress()
-                + "!");
+  public ResponseEntity<Object> health(@RequestHeader String host) {
+    return ResponseEntity.ok().body("Hello Loan stupid! From " + host);
   }
 }
